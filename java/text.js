80da8d3892a0452df1132cedcf809e56e725bcf6
MrsBowie <124260064+MrsBowie@users.noreply.github.com>
2025-05-22 00:55:52 +0200

    more more

---
+++ java/text.js
@@ -0,0 +1,9 @@
+
+function popup() {
+  alert("Denne funktion er desværre gået ned, vi arbejder på at fikse den så hurtigt som muligt!");
+}
+
+function display() {
+    document.getElementById("maintekst").style.display = "none";
+    document.getElementById("engelsktekst").style.display = "visible";
+}

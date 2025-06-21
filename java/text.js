1c681444215af7f2ce7be9379edd9d619c7c82e4
MrsBowie <124260064+MrsBowie@users.noreply.github.com>
2025-06-21 16:44:01 +0200

    more
    text er gud, video mangler ting

--- java/text.js
+++ java/text.js
@@ -3,7 +3,21 @@ function popup() {
   alert("Denne funktion er desværre gået ned, vi arbejder på at fikse den så hurtigt som muligt!");
 }
 
-function display() {
+function displayeng() {
     document.getElementById("maintekst").style.display = "none";
-    document.getElementById("engelsktekst").style.display = "visible";
+    document.getElementById("engelsktekst").style.display = "block";
+    document.getElementById("sprog").style.backgroundColor = "#984670";
+    document.getElementById("sprog2").style.backgroundColor = "#f764b0";
+    
+    document.getElementByClass("goback").innerHTML = "Go back";
 }
+
+function displayda() {
+    document.getElementById("maintekst").style.display = "block";
+    document.getElementById("engelsktekst").style.display = "none";
+    document.getElementById("sprog2").style.backgroundColor = "#984670";
+    document.getElementById("sprog").style.backgroundColor = "#f764b0";
+
+    document.getElementByClass("goback").innerHTML = "Tilbage";
+}
+
--- java/video.js
+++ java/video.js
@@ -1,14 +1,32 @@
 
-let vid = document.getElementById("endovid"); 
 
-function playvid() { 
-  vid.play(); 
-} 
+function vid1() {
+  document.getElementById("endovid2").style.display = "none";
+  document.getElementById("goback").style.display = "none";
+  document.getElementById("buttonx").style.display = "block";
+  document.getElementById("endovid").style.width = "60%";
+  document.getElementById("endovid").style.height = "auto";
 
-function pausevid() { 
-  vid.pause(); 
-} 
+  vid1.play();
+}
 
-function anime() {
-    vid.currentTime = 26;
+function vid2() {
+  document.getElementById("endovid").style.display = "none";
+  document.getElementById("goback").style.display = "none";
+  document.getElementById("buttonx").style.display = "block";
+  document.getElementById("endovid2").style.width = "60%";
+  document.getElementById("endovid2").style.height = "auto";
+
+  vid2.play();
+}
+
+function videoscreen() {
+  document.getElementById("endovid").style.display = "block";
+  document.getElementById("endovid2").style.display = "block";
+  document.getElementById("goback").style.display = "block";
+  document.getElementById("buttonx").style.display = "none";
+  document.getElementById("endovid").style.width = "40%";
+  document.getElementById("endovid").style.height = "auto";
+  document.getElementById("endovid2").style.width = "40%";
+  document.getElementById("endovid2").style.height = "auto";
 }
